e3c95c8c03c1603f5db9e3203edaa0355770ce59
trivuniceva <trivunic99@gmail.com>
2026-01-30 21:58:56 +0100

    standardni problemi resloved

--- client/src/app/components/auth/signup-form/signup-form.component.spec.ts
+++ client/src/app/components/auth/signup-form/signup-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupFormComponent } from './signup-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 describe('SignupFormComponent', () => {
   let component: SignupFormComponent;
@@ -8,10 +9,13 @@ describe('SignupFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupFormComponent]
+      imports: [
+        SignupFormComponent,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SignupFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
