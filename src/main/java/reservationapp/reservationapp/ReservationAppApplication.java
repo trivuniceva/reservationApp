f97753829930fdf866394f60cf470228f809bc07
trivuniceva <trivunic99@gmail.com>
2026-01-30 18:23:02 +0100

    user module

--- src/main/java/reservationapp/reservationapp/ReservationAppApplication.java
+++ src/main/java/reservationapp/reservationapp/ReservationAppApplication.java
@@ -2,8 +2,11 @@ package reservationapp.reservationapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import usermodule.usermodule.UserModuleApplication;
 
 @SpringBootApplication
+@Import(UserModuleApplication.class)
 public class ReservationAppApplication {
 
     public static void main(String[] args) {
